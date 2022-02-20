1b148242c34b8ff772184072fe5ad65ea1f76ebe
sspja <p.janusauskas@sekasoft.com>
2022-02-20 14:50:27 +0200

    add button which changes text in textview

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.fthelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView tvMain;
+    private boolean isHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvMain = (TextView) findViewById(R.id.tvMain);
+        tvMain.setText(R.string.hello);
+        isHelloWorld = true;
+    }
+    public void onClickButtonMain(View v) {
+        if(isHelloWorld) {
+            this.tvMain.setText(R.string.secondary_text);
+        } else {
+            this.tvMain.setText(R.string.hello);
+        }
+        this.isHelloWorld = !this.isHelloWorld;
     }
 }
